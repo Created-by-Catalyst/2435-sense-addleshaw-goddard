318a4d7ecba89ecb1dfd8e2e2f4bcd74d2494852
Sam PC <sammerrick@createdbycatalyst.com>
2024-05-14 10:30:38 +0100

    Debugging character select

--- Endless Runner/Assets/Demo Package/Scripts/GameManager/LoadoutState.cs
+++ Endless Runner/Assets/Demo Package/Scripts/GameManager/LoadoutState.cs
@@ -164,19 +164,20 @@ public class LoadoutState : AState
             bool interactable = ThemeDatabase.loaded && CharacterDatabase.loaded;
             if (interactable)
             {
-                runButton.interactable = true;
-                runButton.GetComponentInChildren<Text>().text = "Run!";
-
-                //we can always enabled, as the parent will be disabled if tutorial is already done
-                //tutorialPrompt.SetActive(true);
-
-                Debug.Log(CharacterDatabase.dictionary.Count);
+                Debug.Log("Total Characters in Database is " + CharacterDatabase.dictionary.Count);
+                Debug.Log("Current Selected Character is  " + PlayerData.instance.characters[PlayerData.instance.usedCharacter]);
 
                 //Add all available characters - once character database has been built <<<< !!!
                 foreach (KeyValuePair<string, Character> characterFromDatabase in CharacterDatabase.dictionary)
                 {
                     PlayerData.instance.AddCharacter(characterFromDatabase.Key);
                 }
+
+                runButton.interactable = true;
+                runButton.GetComponentInChildren<Text>().text = "Run!";
+
+                //we can always enabled, as the parent will be disabled if tutorial is already done
+                //tutorialPrompt.SetActive(true);
             }
         }
 
